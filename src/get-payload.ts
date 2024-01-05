1ef9dc86dce1857c1091ed67bc99f07983c5d7e0
DiNeeZ <dneez86@gmail.com>
2024-01-05 14:04:20 +0200

    feat: auth flow

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,11 +1,23 @@
 import dotenv from "dotenv";
 import path from "path";
 import payload from "payload";
+import nodemailer from "nodemailer";
+
 import type { Payload } from "payload";
 import type { InitOptions } from "payload/config";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
+});
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
 });
 
 let cached = (global as any).payload;
@@ -32,6 +44,11 @@ export const getPayloadClient = async ({
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "maiorov.den.fe@gmail.com",
+        fromName: "Diggy Hippo",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
